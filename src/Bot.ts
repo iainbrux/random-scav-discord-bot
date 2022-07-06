d5c83de69dd5ca76433de25b0f68ef1b797b3e6a
Iain Brookes <iain.brookes@pimberly.com>
2022-07-06 18:54:28 +0100

    setup repo & .env vars, test bot is working, will now branch off master

---
+++ src/Bot.ts
@@ -0,0 +1,12 @@
+import { Client } from "discord.js";
+import { config } from "./config";
+
+console.log("Bot is starting...");
+
+const client = new Client({
+  intents: [],
+});
+
+client.login(config.token);
+
+console.log("Succesfully logged in...");
