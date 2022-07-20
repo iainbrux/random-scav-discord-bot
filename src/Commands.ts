45ec920c83e39782b02f61246690e7d74d9c6da3
Hakan Cimen <hakan.cimen@pimberly.com>
2022-07-20 20:57:58 +0100

    removed unwanted commands

--- src/Commands.ts
+++ src/Commands.ts
@@ -1,6 +1,4 @@
 import { Command } from "./Command";
-import { Bye } from "./commands/Bye";
-import { Hello } from "./commands/Hello";
 import { Join } from "./commands/Join";
 
-export const Commands: Command[] = [Hello, Bye, Join];
+export const Commands: Command[] = [Join];
